6e7263da94d5073ff2309045e6b227bcc36bae93
Tanvir0234 <tanvir.ali0234@gmail.com>
2024-09-27 15:29:48 +0600

    error

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -5,6 +5,7 @@ import PropTypes from 'prop-types'
 const Blogs = ({handleAddToBookmark}) => {
 
     const [blogs,setBlogs] = useState([])
+    console.log(blogs)
 
     useEffect(()=>{
         fetch('blogs.json')
@@ -20,7 +21,7 @@ const Blogs = ({handleAddToBookmark}) => {
     <div className="md:w-2/3">
     <h1 className="text-4xl">Blogs : {blogs.length}</h1>
     {
-        blogs.map(blog => <Blog 
+        blogs?.map(blog => <Blog 
             key={blog.id} 
             blog={blog}>
             handleAddToBookmark={handleAddToBookmark}
--- src/components/Bookmarks/Bookmarks.jsx
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -2,12 +2,13 @@ import Bookmark from "../Bookmark/Bookmark";
 
 
 const Bookmarks = ({bookmarks}) => {
+    console.log(bookmarks)
     return (
         <div className="md:w-1/3">
             <p>Bookmarked Blogs : {bookmarks.length}</p>
             {
-                bookmarks.map(bookmark => <Bookmark 
-                    key={bookmark} bookmark={bookmark}></Bookmark>)
+                bookmarks?.map(bookmark => <Bookmark 
+                    key={bookmark.id} bookmark={bookmark}></Bookmark>)
             }
         </div>
     );
